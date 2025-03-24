2a3fd2abac633f2ad79f71a580064fd67ead4b24
widson <widson.venancio@etec.sp.gov.br>
2025-03-24 20:40:11 -0300

    mais condições

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
-$teste
+$valor=1;
+$valor1=-1;
+if ($valor>0 || $valor1>0){
+    echo "verdadeiro";
+
+} 
+else {
+echo "falso";
+
+}
+
+switch ($variavel) {
+    case 'a';
+    break;
+
+    case 'b';
+    break;
+
+    case 'c';
+    break;
+}
+
+?>
